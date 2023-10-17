8717a233f76d3f094f010f12dc6ac6b601914f60
frbarbre <fr.barbre@gmail.com>
2023-10-17 15:03:13 +0200

    deploy test

--- components/shared/GameCard.jsx
+++ components/shared/GameCard.jsx
@@ -7,19 +7,40 @@ export default async function GameCard({ game }) {
     `fields image_id; where game = ${game?.id};`
   );
 
-  console.log(cover);
+  const genres = await fetchGameData(
+    "genres",
+    `fields name; where id = (${game?.genres});`
+  );
+
+  const genre =
+    genres[0]?.name === "Role-playing (RPG)"
+      ? "RPG"
+      : genres[0]?.name === "Real Time Strategy (RTS)"
+      ? "RTS"
+      : genres[0]?.name === "Turn-based strategy (TBS)"
+      ? "TBS"
+      : genres[0]?.name === "Hack and slash/Beat 'em up"
+      ? "Hack'n'slash"
+      : genres[0]?.name === "Card & Board Game"
+      ? "Board Game"
+      : genres[0]?.name === "Point-and-click"
+      ? "Point & click"
+      : genres[0]?.name;
 
   return (
     <>
       {cover && (
-        <Link href={`/games/${game.slug}`}>
+        <Link href={`/games/${game.slug}`} className="block">
           <img
             src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${cover[0]?.image_id}.png`}
             alt={`${game?.name}-cover`}
+            className="rounded-t-[2px] w-[160px] aspect-[160/214] object-cover"
           />
           <section>
-            <h3>{game?.name}</h3>
-            <p>{game?.slug}</p>
+            <div>
+              <h3>{game?.name}</h3>
+              <p>{genre}</p>
+            </div>
           </section>
         </Link>
       )}
