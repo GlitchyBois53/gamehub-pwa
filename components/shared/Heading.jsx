8fb30c85fcceb05e5bef3e0707ba31d178a23bc6
frbarbre <fr.barbre@gmail.com>
2023-11-01 10:17:17 +0100

    fixed heading when not logged in

--- components/shared/Heading.jsx
+++ components/shared/Heading.jsx
@@ -2,18 +2,26 @@ import Link from "next/link";
 
 export default function Heading({ text, image, username, clerkId }) {
   return (
-    <Link
-      href={`/profile/${clerkId}`}
-      className="cursor-pointer flex gap-[12px] items-center mb-[32px]"
-    >
-      <img
-        src={image}
-        alt={`${username}-profile-photo`}
-        className="w-[36px] aspect-square rounded-full object-cover"
-      />
-      <h1 className="text-[32px] font-bold tracking-[1.92px] uppercase bg-game-grad bg-clip-text text-transparent w-max">
-        {username}'s {text}
-      </h1>
-    </Link>
+    <>
+      {username ? (
+        <Link
+          href={`/profile/${clerkId}`}
+          className="cursor-pointer flex gap-[12px] items-center mb-[32px]"
+        >
+          <img
+            src={image}
+            alt={`${username}-profile-photo`}
+            className="w-[36px] aspect-square rounded-full object-cover"
+          />
+          <h1 className="text-[32px] font-bold tracking-[1.92px] uppercase bg-game-grad bg-clip-text text-transparent w-max">
+            {username}'s {text}
+          </h1>
+        </Link>
+      ) : (
+        <h1 className="text-[32px] font-bold tracking-[1.92px] uppercase bg-game-grad bg-clip-text text-transparent w-max mb-[32px]">
+          {text}
+        </h1>
+      )}
+    </>
   );
 }
