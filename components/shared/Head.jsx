d0a8b94c43832dc424ca2b75d126f3d040ede153
frbarbre <fr.barbre@gmail.com>
2023-11-20 11:42:49 +0100

    converted to webapp

--- components/shared/Head.jsx
+++ components/shared/Head.jsx
@@ -1,15 +1,15 @@
 // Programmed in Collaboration by: Dennis Russell, Lean Hansen & Frederik Barbré
 
-"use client";
+'use client';
 
-import { usePathname } from "next/navigation";
-import { useStore } from "../../app/store";
+import { usePathname } from 'next/navigation';
+import { useStore } from '../../app/store';
 
 // This component is used to display the head of the page
 export default function Head() {
   const headText = useStore((store) => store.headTitle);
   const pathname = usePathname();
-  const pathSplit = pathname.split("/");
+  const pathSplit = pathname.split('/');
   let path = pathSplit[1];
 
   if (pathSplit.length === 3) {
@@ -18,17 +18,18 @@ export default function Head() {
   return (
     <head>
       <title>
-        {pathname === "/"
-          ? "Home - GameHub"
-          : pathname.includes("/games") && pathname !== "/games"
+        {pathname === '/'
+          ? 'Home - GameHub'
+          : pathname.includes('/games') && pathname !== '/games'
           ? `${path} - GameHub`
-          : path[0].toUpperCase() + path.slice(1).toLowerCase() + " - GameHub"}
+          : path[0].toUpperCase() + path.slice(1).toLowerCase() + ' - GameHub'}
       </title>
       {/* TODO: add SEO if needed */}
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1"
       ></meta>
+      <link rel="manifest" href="/manifest.json" />
     </head>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,27 +1,43 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === 'development',
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
+});
+
+module.exports = withPWA(nextConfig);
+
 const nextConfig = {
   experimental: {
     serverActions: true,
-    serverComponentsExternalPackages: ["mongoose"],
-    serverActionsBodySizeLimit: "4mb",
+    serverComponentsExternalPackages: ['mongoose'],
+    serverActionsBodySizeLimit: '4mb',
   },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "img.clerk.com",
+        protocol: 'https',
+        hostname: 'img.clerk.com',
       },
       {
-        protocol: "https",
-        hostname: "images.clerk.dev",
+        protocol: 'https',
+        hostname: 'images.clerk.dev',
       },
       {
-        protocol: "https",
-        hostname: "uploadthing.com",
+        protocol: 'https',
+        hostname: 'uploadthing.com',
       },
       {
-        protocol: "https",
-        hostname: "placehold.co",
+        protocol: 'https',
+        hostname: 'placehold.co',
       },
     ],
   },
