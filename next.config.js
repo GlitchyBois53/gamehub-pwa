de9c824843e58193cfde17203f906df1f43228f0
frbarbre <fr.barbre@gmail.com>
2023-11-20 12:24:35 +0100

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,42 +1,44 @@
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === 'development',
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
+});
+
 /** @type {import('next').NextConfig} */
-
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
-
-module.exports = withPWA({
-  reactStrictMode: true,
-});
-
 const nextConfig = {
+  // ... other options you like
   experimental: {
     serverActions: true,
-    serverComponentsExternalPackages: ["mongoose"],
-    serverActionsBodySizeLimit: "4mb",
+    serverComponentsExternalPackages: ['mongoose'],
+    serverActionsBodySizeLimit: '4mb',
   },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "img.clerk.com",
+        protocol: 'https',
+        hostname: 'img.clerk.com',
       },
       {
-        protocol: "https",
-        hostname: "images.clerk.dev",
+        protocol: 'https',
+        hostname: 'images.clerk.dev',
       },
       {
-        protocol: "https",
-        hostname: "uploadthing.com",
+        protocol: 'https',
+        hostname: 'uploadthing.com',
       },
       {
-        protocol: "https",
-        hostname: "placehold.co",
+        protocol: 'https',
+        hostname: 'placehold.co',
       },
     ],
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
